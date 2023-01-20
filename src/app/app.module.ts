27c09ec6decfc1592cdd0ebbccb067b029bf1899
052-Usman <usmanvirk052@gmail.com>
2023-01-20 22:54:37 +0500

    navbar+routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './componets/nav-bar/nav-bar.component';
+import { ProfileComponent } from './componets/profile/profile.component';
+import { HomeComponent } from './componets/home/home.component';
+import { TaskComponent } from './componets/task/task.component';
+import { AboutComponent } from './componets/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ProfileComponent,
+    HomeComponent,
+    TaskComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'task', component: TaskComponent },
+      { path: 'about', component: AboutComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
