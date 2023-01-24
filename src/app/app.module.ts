3335cdd3ab040e7f0c4fc46a6e13d00e840564f8
052-Usman <usmanvirk052@gmail.com>
2023-01-24 09:47:41 +0500

    day3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,6 +31,7 @@ import { UserComponent } from './componets/user/user.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
--- src/app/componets/profile/profile.component.ts
+++ src/app/componets/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  name: string | undefined;
+  email: string | undefined;
+  age: string | undefined;
+
+  onSubmitCheck: boolean = false;
+
+  userProfileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    age: new FormControl('', Validators.required),
+  });
+
+  inputF = document.getElementById("id1");
+
+  onSubmit() {
+    console.log('Name: '+ this.userProfileForm.value['name']);
+    console.log('Email: '+ this.userProfileForm.value['email']);
+    console.log('Age: '+ this.userProfileForm.value['age']);
+
+    this.onSubmitCheck = true;
+
+    this.name = 'Name: '+ this.userProfileForm.value['name'];
+    this.email = 'Eamil: '+ this.userProfileForm.value['email'];
+    this.age = 'Age: '+ this.userProfileForm.value['age'];
+    
+    console.warn(this.userProfileForm.value);
+    
+    this.userProfileForm.reset();
+
+    }
 }
